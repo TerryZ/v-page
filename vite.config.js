59568ac4fa6c0f7111cd2d84e1f41a3a9a540119
Terry <terry5@foxmail.com>
2022-08-24 14:32:00 +0800

    chore: 增加 coverage 报告输出

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-// import { defineConfig } from 'vitest/config'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
@@ -19,6 +19,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {},
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html']
     }
   }
 })
