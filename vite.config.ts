9bcc45f78a56a74847f5201bc9752a1db69e34e7
Terry <terry5@foxmail.com>
2026-01-08 00:00:22 +0800

    chore: dts 文档构建

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import css from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), css()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    css(),
+    dts({
+      entryRoot: 'src',
+      // rollupTypes: true,
+      tsconfigPath: './tsconfig.app.json',
+      outDir: './dist/types/',
+      exclude: ['src/__test__/**']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,9 +27,9 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'VPage',
-      formats: ['es', 'umd'],
+      formats: ['es', 'cjs'],
       fileName: 'v-page'
     },
     rollupOptions: {
