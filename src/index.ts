ddff03424a954b942dc56328d18eed45be96f1b7
Terry <terry5@foxmail.com>
2026-01-07 23:51:24 +0800

    feat: 全局安装新增 register 选项，以控制是否全局注册组件

--- src/index.ts
+++ src/index.ts
@@ -6,28 +6,30 @@ import { EN } from './language'
 import { keyOptions } from './constants'
 import type { LanguageKey, PaginationGlobalOptions } from './types'
 
-export type * from './types'
+export * from './types'
 export * from './PaginationCore'
 
 const PluginSetup = () => {
   const lang = ref(EN)
 
-  const install = (app: App, options: PaginationGlobalOptions = {}) => {
-    app.provide(keyOptions, lang)
-    if (options?.language) setLanguage(options.language)
-    app.component(PaginationBar.name!, PaginationBar)
+  const PaginationPlugin: Plugin<[PaginationGlobalOptions?]> = {
+    install(app: App, options?: PaginationGlobalOptions) {
+      app.provide(keyOptions, lang)
+      if (options?.language) {
+        setLanguage(options.language)
+      }
+      if (options?.register) {
+        app.component(PaginationBar.name!, PaginationBar)
+      }
+    }
   }
   const setLanguage = (language?: LanguageKey) => {
     lang.value = language || EN
   }
-  return { install, setLanguage }
+  return { PaginationPlugin, setLanguage }
 }
 
-const { install, setLanguage } = PluginSetup()
-
-const PaginationPlugin: Plugin<PaginationGlobalOptions> = {
-  install
-}
+const { PaginationPlugin, setLanguage } = PluginSetup()
 
 export { PaginationBar, setLanguage }
 export default PaginationPlugin
